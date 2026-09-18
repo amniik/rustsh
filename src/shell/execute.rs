ae3eeede1ef2b0b9d69ccca7b9b5422b76372519
Amir Nikpour <amnik.free@gmail.com>
2026-09-18 13:47:09 +0330

    shell: add type built-in command

--- src/shell/execute.rs
+++ src/shell/execute.rs
@@ -1,9 +1,16 @@
+use std::path::PathBuf;
+use std::{env, os::unix::fs::PermissionsExt};
+
 /// Returns `true` when the shell should exit.
 pub fn execute(args: &[String]) -> bool {
     match args[0].as_str() {
         "exit" => true,
         "echo" => {
             echo(&args[1..]);
+            false
+        }
+        "type" => {
+            type_cmd(&args[1..]);
             false
         }
         _ => {
@@ -15,4 +22,39 @@ pub fn execute(args: &[String]) -> bool {
 
 fn echo(args: &[String]) {
     println!("{}", args.join(" "));
+}
+
+fn is_builtin_commad(command: &str) -> bool {
+    matches!(command, "echo" | "type" | "exit")
+}
+
+fn find_executable(name: &str) -> Option<PathBuf> {
+    let path = env::var_os("PATH")?;
+
+    for dir in env::split_paths(&path) {
+        let candidate = dir.join(name);
+
+        if candidate.is_file()
+            && candidate
+                .metadata()
+                .map(|m| m.permissions().mode() & 0o111 != 0)
+                .unwrap_or(false)
+        {
+            return Some(candidate);
+        }
+    }
+
+    None
+}
+
+fn type_cmd(args: &[String]) {
+    for arg in args {
+        if is_builtin_commad(arg) {
+            println!("{arg} is a shell builtin");
+        } else if let Some(command) = find_executable(arg) {
+            println!("{arg} is {}", command.display());
+        } else {
+            println!("{}: not found", arg);
+        }
+    }
 }
--- tests/built_in_cmd.rs
+++ tests/built_in_cmd.rs
@@ -21,6 +21,63 @@ fn test_echo_command() {
 }
 
 #[test]
+fn test_type_builtin() {
+    let mut child = Command::new(env!("CARGO_BIN_EXE_rustsh"))
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    child
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all(b"type echo\n")
+        .unwrap();
+    let output = child.wait_with_output().unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    assert!(stdout.contains("echo is a shell builtin"));
+}
+
+#[test]
+fn test_type_unknown_command() {
+    let mut child = Command::new(env!("CARGO_BIN_EXE_rustsh"))
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    child
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all(b"type hello\n")
+        .unwrap();
+    let output = child.wait_with_output().unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    assert!(stdout.contains("hello: not found"));
+}
+
+#[test]
+fn test_type_external_command() {
+    let mut child = Command::new(env!("CARGO_BIN_EXE_rustsh"))
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    child
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all(b"type ls\n")
+        .unwrap();
+    let output = child.wait_with_output().unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    assert!(stdout.contains("ls is /usr/bin/ls"));
+}
+
+#[test]
 fn test_exit_command() {
     let mut child = Command::new(env!("CARGO_BIN_EXE_rustsh"))
         .stdin(Stdio::piped())
