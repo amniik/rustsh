9e4137afe7197dbf633c0b1e6a58d39943a5ae60
Amir Nikpour <amnik.free@gmail.com>
2026-09-18 12:06:09 +0330

    shell: Add simple REPL
    Simple REPL(Read, Eval, Print, Loop) is implemented for rusth.
    This is the core of each shell.

--- src/shell.rs
+++ src/shell.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 pub struct Shell {}
 
 impl Shell {
@@ -6,11 +8,39 @@ impl Shell {
     }
 
     pub fn run(&mut self) {
-        self.print_prompt();
+        loop {
+            self.print_prompt();
+            let Some(cmd) = self.read_input() else {
+                break;
+            };
+            let args = self.parser(&cmd);
+            self.eval(&args);
+        }
     }
 
     fn print_prompt(&self) {
-        print!("$ ")
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    }
+
+    fn read_input(&self) -> Option<String> {
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        if command.is_empty() {
+            return None;
+        }
+        Some(command.trim().to_string())
+    }
+
+    fn parser(&self, command: &str) -> Vec<String> {
+        command.split_whitespace().map(String::from).collect()
+    }
+
+    fn eval(&self, args: &[String]) {
+        if args.is_empty() {
+            return;
+        }
+        println!("{}: command not found", args[0])
     }
 }
 
